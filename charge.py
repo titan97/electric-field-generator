ad25ae8b1287e9367d0033a914835d07133fbfa1
titan97 <22862806+titan97@users.noreply.github.com>
2022-09-06 22:19:46 -0700

    initial commit

---
+++ charge.py
@@ -0,0 +1,10 @@
+class Charge:
+    def __init__(self, x, y, q):
+        """
+        creates a charge object
+        at position x and position y
+        with charge q
+        """
+        self.x = x
+        self.y = y
+        self.q = q
