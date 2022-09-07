1682bb3f8c01151013649fa50a36a2f8d62e1431
titan97 <22862806+titan97@users.noreply.github.com>
2022-09-06 22:45:18 -0700

    Update graphfield.py

--- graphfield.py
+++ graphfield.py
@@ -6,14 +6,10 @@ from elecfield import drawField
 charges = []
 
 # Ask user for charge particle details
-
-x, y, q = input(
-    'Please enter charge particle values in the following format separated by spaces: x_position, y_position, charge_value \n').split()
-
-print("x position: ", x)
-print("y position: ", y)
-print("charge: ", q)
-
-Q = Charge(int(x), int(y), int(q))
-charges.append(Q)
+n = int(input('Please enter the number of charges to add on the graph: '))
+for i in range(n):
+    x, y, q = input(
+        'Please enter charge particle values in the following format separated by spaces: x position, y position, charge value \n').split()
+    Q = Charge(int(x), int(y), int(q))
+    charges.append(Q)
 drawField(4, 4, 0.2, charges)
